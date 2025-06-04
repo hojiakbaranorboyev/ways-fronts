af4fd2b716eacf68d11bd08b7d98628f06e571a9
Hojiakbar Anorboyev <k.anorboyev@royaltaxi.uz>
2025-06-04 13:46:14 +0500

    configs

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,8 @@
 import { RippleButton } from "ui";
 function App() {
+  
   return (
-    <div className="p-4 bg-yellow-200 text-xl font-semibold">
+    <div className="p-4 bg-yellow-200 text-xl font-semibold">asdasd
       <RippleButton>asd</RippleButton>
     </div>
   );
